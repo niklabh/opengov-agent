ebb0e0a49ce6d0361bbfc4b48106f2505b9aeff7
Nikhil Ranjan <niklabh811@gmail.com>
2025-02-27 02:38:21 +0000

    Restored to '14a3c2d6c223915c29e950cfaf39f8d91ded580a'
    Replit-Restored-To:14a3c2d6c223915c29e950cfaf39f8d91ded580a

--- server/index.ts
+++ server/index.ts
@@ -54,7 +54,7 @@ app.use((req, res, next) => {
       serveStatic(app);
     }
 
-    const port = process.env.PORT || 5001;
+    const port = 5000;
     server.listen({
       port,
       host: "0.0.0.0",
